524c836c1ecee9ec033f65d0a96683777e546f0e
numlims <max.haubold@med.uni-greifswald.de>
2024-12-03 15:41:55 +0100

    pyproject

---
+++ dq/__main__.py
@@ -0,0 +1,18 @@
+from dq import dq
+import sys
+
+
+def main()
+  target = sys.argv[1]
+  operation = sys.argv[2]
+
+  d = dq(target)
+
+  if operation == "select":
+      path = sys.argv[3]
+      where = sys.argv[4]
+      print(d.select(path, where))
+
+
+sys.exit(main())
+
